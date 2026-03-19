746c9b59459bda460164add03a56d80e36c59eab
shayake <shayakkhan82@gmail.com>
2026-03-20 01:39:55 +0530

    final cors fix

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ connectDB()
 
 //middleware to handle CORS
 
-// app.use(
-//     cors({
-//         origin:"*",
-//         methods:["GET","POST","PUT","DELETE"],
-//         allowedHeaders:["Content-Type","Authorization"],
-//         credentials:true,
-//     })
-// )
+app.use(
+    cors({
+        origin:"*",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"],
+        credentials:true,
+    })
+)
 // app.use(
 //   cors({
 //     origin: ["http://localhost:5173"], // your frontend
@@ -45,18 +45,18 @@ connectDB()
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ai-power-learner-frontend.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-app.options("*", cors());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ai-power-learner-frontend.vercel.app"
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
