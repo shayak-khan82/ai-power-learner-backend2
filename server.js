6d5fb283bf35282d0301ca3732bf15827c50f5c8
shayake <shayakkhan82@gmail.com>
2026-03-21 00:42:20 +0530

    fix cors final working

--- server.js
+++ server.js
@@ -45,17 +45,23 @@ connectDB()
 //     credentials: true,
 //   })
 // );
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ai-power-learner-frontend.vercel.app"
-    ],
-   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ai-power-learner-frontend.vercel.app"
+//     ],
+//    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: true,   // 🔥 allow all origins dynamically
+  credentials: true
+}));
+
+app.options("*", cors());
 
 
 app.use(express.json())
