3b892458f449a5cd42559fd145983abb079ce8ea
shayake <shayakkhan82@gmail.com>
2026-03-20 17:23:14 +0530

    chabge port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(
       "http://localhost:5173",
       "https://ai-power-learner-frontend.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
